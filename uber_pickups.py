9fa95cbd41a4432d28b7949150311478d8130092
PMootz <philmootz9@gmail.com>
2024-02-03 18:18:58 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -8,8 +8,8 @@ import base64
 import ccloud_lib
 from confluent_kafka import Consumer
 import time
-
 from kafka import KafkaConsumer
+
 CONF = ccloud_lib.read_ccloud_config("python.config")
 TOPIC = "spaceship_titanic_data" 
 
@@ -72,30 +72,10 @@ if(api_clicked):
         age = response_data.get('Age', 20.0)
         vip = response_data.get('VIP', False)  
         roomService = response_data.get('RoomService', 0)  
-        try:
-            roomService = int(float(roomService))
-        except ValueError:
-            roomService = 0
         foodCourt = response_data.get('FoodCourt', 0)
-        try:
-            foodCourt= int(float(foodCourt))
-        except ValueError:
-            foodCourt = 0
         shoppingMall = response_data.get('ShoppingMall', 0)
-        try:
-            shoppingMall = int(float(shoppingMall))
-        except ValueError:
-            shoppingMall = 0
         spa = response_data.get('Spa', 0)
-        try:
-            spa= int(float(spa))
-        except ValueError:
-            spa= 0
         vrDeck = response_data.get('VRDeck', 0)
-        try:
-            vrDeck= int(float(vrDeck))
-        except ValueError:
-            vrDeck = 0
         #transported = response_data.get('Transported', False)   
     else:
         # Print an error message if the request was not successful
@@ -103,8 +83,6 @@ if(api_clicked):
 
 button_clicked  = st.button("Check the survival rate")
 
-
-kafkaC=True
 kafkaApi =""
 kafkaApiS=""
 kafkaS=""
@@ -118,27 +96,23 @@ else:
 encoded_secret_variableK = os.environ.get("KAFKA_API")
 if encoded_secret_variableK:
     kafkaApi= encoded_secret_variableK
-else:
-    kafkaC=False
 
 encoded_secret_variableKS = os.environ.get("KAFKA_API_SECRET")
 if encoded_secret_variableKS:
     kafkaApiS= encoded_secret_variableKS
-else:
-    kafkaC=False
     
 encoded_secret_variableKSs = os.environ.get("KAFKA_SERVER")
 if encoded_secret_variableKSs:
     kafkaS= encoded_secret_variableKSs
-else:
-    kafkaC=False
 
 
 # Connect to Kafka
 CLUSTER_API_KEY = kafkaApi
 CLUSTER_API_SECRET = kafkaApiS
 bootstrap_servers = kafkaS
-if kafkaC:
+
+askOnceKafka= st.button("Ask Kafka for one data")
+if askOnceKafka:
     consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(CONF)
     consumer_conf['group.id'] = 'python-group-1'
     consumer_conf['auto.offset.reset'] = 'earliest' # This means that you will consume latest messages that your script haven't consumed yet!
@@ -147,72 +121,29 @@ if kafkaC:
     # Subscribe to topic
     consumer.subscribe([TOPIC])
     try:
-        while True:
-            msg = consumer.poll(5.0) # Search for all non-consumed events. It times out after 5 second
-            if msg is None:
-                print("Waiting for message or event/error in poll()")
-                continue
-            elif msg.error():
-                st.write('error: {}'.format(msg.error()))
-            else:
+        msg = consumer.poll(5.0) # Search for all non-consumed events. It times out after 5 second
+        if msg is None:
+            print("Waiting for message or event/error in poll()")
+        elif msg.error():
+            st.write('error: {}'.format(msg.error()))
+        else:
                 # Check for Kafka message
-                record_key = msg.key()
-                record_value = msg.value()
-                data = json.loads(record_value)
-                sy.write(data)
-                newdata = True
-                passenger = data.get('PassengerId', 'N/A')
-                name = data.get('Name', 'Unknown')
-                homePlanet = data.get('HomePlanet', 'Earth')
-                cryptoSleep = data.get('CryoSleep', False)
-                cabin = data.get('Cabin', 'N/A')
-                destination = data.get('Destination', 'TRAPPIST-1e')
-                age = data.get('Age', 20.0)
-                vip = data.get('VIP', False)  
-                roomService = data.get('RoomService', 0)  
-                time.sleep(10) # Wait 10  second
-    except KeyboardInterrupt:
-        pass
-    finally:
-        # Leave group and commit final offsets
-        consumer.close()
-else:
-    CLUSTER_API_KEY  = 'PVOPH4N5P77FTZMI'
-    CLUSTER_API_SECRET = 'hYsgWo6H52afMzq1Az3iGn6gC7aD4A/jNU0H//QCtmoz1T3njqg8ZMCKOf960dd+'
-    consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(CONF)
-    consumer_conf['group.id'] = 'python-group-1'
-    consumer_conf['auto.offset.reset'] = 'earliest' # This means that you will consume latest messages that your script haven't consumed yet!
-    consumer = Consumer(consumer_conf)
-    st.write("Data from Kafka")
-    # Subscribe to topic
-    consumer.subscribe([TOPIC])
-    try:
-        while True:
-            msg = consumer.poll(5.0) # Search for all non-consumed events. It times out after 5 second
-            if msg is None:
-                print("Waiting for message or event/error in poll()")
-                continue
-            elif msg.error():
-                st.write('error: {}'.format(msg.error()))
+            record_value = msg.value()
+            datat = json.loads(record_value)
+            if not(datat['passenger_informations']):
+                data=datat
             else:
-                # Check for Kafka message
-                record_key = msg.key()
-                record_value = msg.value()
-                data = json.loads(record_value)
-                st.write(data)
-                newdata = True
-                passenger = data.get('PassengerId', 'N/A')
-                name = data.get('Name', 'Unknown')
-                homePlanet = data.get('HomePlanet', 'Earth')
-                cryptoSleep = data.get('CryoSleep', False)
-                cabin = data.get('Cabin', 'N/A')
-                destination = data.get('Destination', 'TRAPPIST-1e')
-                age = data.get('Age', 20.0)
-                vip = data.get('VIP', False)  
-                roomService = data.get('RoomService', 0)  
-                time.sleep(10) # Wait 10  second
-    except KeyboardInterrupt:
-        pass
+                data=datat['passenger_informations']
+            st.write(data)
+            passenger = data.get('PassengerId', 'N/A')
+            name = data.get('Name', 'Unknown')
+            homePlanet = data.get('HomePlanet', 'Earth')
+            cryptoSleep = data.get('CryoSleep', False)
+            cabin = data.get('Cabin', 'N/A')
+            destination = data.get('Destination', 'TRAPPIST-1e')
+            age = data.get('Age', 20.0)
+            vip = data.get('VIP', False)  
+            roomService = data.get('RoomService', 0)  
     finally:
         # Leave group and commit final offsets
         consumer.close()
@@ -224,6 +155,28 @@ if(name==''):
     name='Unknow'
 if(cabin ==''):
     cabin='N/A'
+try:
+    roomService = int(float(roomService))
+except ValueError:
+    roomService = 0
+try:
+    foodCourt= int(float(foodCourt))
+except ValueError:
+    foodCourt = 0
+try:
+    shoppingMall = int(float(shoppingMall))
+except ValueError:
+    shoppingMall = 0
+try:
+    spa= int(float(spa))
+except ValueError:
+    spa= 0
+try:
+    vrDeck= int(float(vrDeck))
+except ValueError:
+    vrDeck = 0
+
+total = float(foodCourt+roomService+spa+shoppingMall+vrDeck)
 
 record_to_predict = {
       "PassengerId": passenger,
@@ -241,10 +194,10 @@ record_to_predict = {
       "Total": total,
       "Name": name
 }
-total = float(foodCourt+roomService+spa+shoppingMall+vrDeck)
+
 
 client = dataikuapi.APINodeClient(dataikuApi,"space_titanic_crounch")
-if(button_clicked or api_clicked or newdata):
+if(button_clicked or api_clicked):
     st.table(pd.DataFrame(record_to_predict, index=[0]))
     prediction = client.predict_record("space_titanic_end", record_to_predict)
     if(passenger =='N/A'):
@@ -255,5 +208,4 @@ if(button_clicked or api_clicked or newdata):
     st.text("The passenger " + name + " is " + outcome + " with a probability of "+ str(round(prediction['result']["probas"][prediction['result']["prediction"]],4)*100)+"%")
     st.bar_chart(prediction['result']["probas"])
     api_clicked = False
-    newdata=False
   
